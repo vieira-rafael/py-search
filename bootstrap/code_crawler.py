07911fb5880c0fbfd0293f3a07294a7133a9c390
Thais <thaisnviana@gmail.com>
2016-05-14 15:24:07 -0300

    [bug-fix] code_crawler

--- bootstrap/code_crawler.py
+++ bootstrap/code_crawler.py
@@ -1,58 +1,62 @@
 # _*_ coding:utf-8 _*_
 
-import json
 import traceback
 from bs4 import BeautifulSoup
 import requests
+import json
 
-def get_files(full_name):
-	try:
-		links = []
-		html_page = requests.get('https://github.com/'+full_name)
-		
-		soup = soup_wrap(html_page)
-
-		file_wrap = soup.findAll("div", { "class" : "file-wrap" })
-
-		for row in file_wrap:
-			links = links + row.findAll('a', href = True)
-		
-		for link in links:
-			if ".py" in link['href']:
-				print(link['title'])
-			if 'tree' in link['href']:
-				print(link)
-				if 'title' in link:
-					print(link['title'])
-
-	except UnicodeEncodeError:
-		traceback.print_exc()
-		pass
-
-def get_code(url):
-	print('bela')
+class CodeCrawler:
 
 
-def soup_wrap(url):
-	git_content = str(url.content, "ascii", errors="ignore")
+	def soup_wrap(self, url):
+		git_content = str(url.content, "ascii", errors="ignore")
 
-	soup = BeautifulSoup(git_content, 'html.parser')
-	return soup
+		soup = BeautifulSoup(git_content, 'html.parser')
+		return soup
 
 
-def main():
-    try:
-    	for index in range(1,129):
-    		__file__ = '../data/repos/page'+ str(index)
-	    	with open(__file__, 'r') as jsonfile:
-	    		items_list = json.load(jsonfile)['items']
-	    		for item in items_list:
-	    			get_files(item['full_name'])
-	    			break  
-	    	break		
-    except:
-    	traceback.print_exc()
-    	pass	
+	def get_files(self, full_name):
+		try:
+			links = []
+			html_page = requests.get('https://github.com/'+full_name)
+			
+			soup = self.soup_wrap(html_page)
 
-if __name__ == "__main__":
-    main()
+			file_wrap = soup.findAll("div", { "class" : "file-wrap" })
+
+			for row in file_wrap:
+				links = links + row.findAll('a', href = True)
+			
+			for link in links:
+				if ".py" in link['href']:
+					self.get_code(link['href'])
+				elif 'tree' in link['href'] and link.has_attr('title'):
+					self.get_files(link['href'])
+		except UnicodeEncodeError:
+			traceback.print_exc()
+			pass
+	
+	def get_code(self, url):
+		filename = "data/codes/" +str(url.split('/')[1])+ "_" +str(url.split('/')[-1])
+		print(filename)
+		html_page = requests.get('https://github.com/'+url)		
+		soup = self.soup_wrap(html_page)
+		code_wrap = soup.findAll("td", { "class" : "blob-code" })
+
+		f = open(filename, "w")
+		for row in code_wrap:
+			f.write(row.text)
+		f.close()
+
+	def __init__(self):
+		try:
+			for index in range(1,129):
+				__file__ = 'data/repos/page'+ str(index)
+				with open(__file__, 'r') as jsonfile:
+					for item in json.load(jsonfile)['items']:
+						self.get_files(item['full_name'])
+						break
+				break
+		except:
+			traceback.print_exc()
+			pass
